2e3857a63e4b1451e1b005d38ed6ab7c23e6718a
rsoule3 <robsoule3@gmail.com>
2015-06-28 10:09:26 -0400

    Initial commit

---
+++ queue.js
@@ -0,0 +1,97 @@
+(function () {
+  'use strict';
+
+  function Queue() {
+    var theLine = [],
+      numberInQueue = 0;
+
+    return {
+
+      enqueue: function () {
+        var i;
+        for (i = 0; i < arguments.length; i += 1) {
+          theLine.push(arguments[i]);
+          numberInQueue += 1;
+          console.log(arguments[i] + ' added to the Queue');
+        }
+
+        return theLine;
+      },
+
+      dequeue: function () {
+        numberInQueue -= 1;
+        console.log(theLine[0] + ' is up in the Queue!');
+        return theLine.shift();
+      },
+
+      front: function () {
+        console.log('the next up is ' + theLine[0]);
+        return theLine[0];
+      },
+
+      isEmpty: function () {
+        console.log('Is q empty?: ' + !numberInQueue);
+        return !numberInQueue;
+      },
+
+      size: function () {
+        console.log('There are ' + numberInQueue + ' in the Queue');
+        return numberInQueue;
+      }
+    };
+
+  }
+
+  function PriorityQueue() {
+    var priorityLine = [];
+
+    return {
+
+      enqueue: function (item, priority) {
+        var theItem = {item: item, priority: priority}, i;
+
+        for (i = 0; i < priorityLine.length; i += 1) {
+          if (theItem.priority < priorityLine[i].priority) {
+            priorityLine.splice(i, 0, theItem);
+            console.log('spliced in at middle', item);
+            return priorityLine;
+          }
+        }
+
+        priorityLine.push(theItem);
+        console.log('addedtoEndofQ');
+        return priorityLine;
+      },
+
+      print: function () {
+        console.log(priorityLine);
+      }
+
+    };
+  }
+
+  var prioQ = new PriorityQueue(),
+    simpleQ = new Queue();
+  prioQ.enqueue('loller', 2);
+  prioQ.enqueue('testalest', 3);
+  prioQ.enqueue('first', 1);
+  prioQ.enqueue('second', 1);
+  prioQ.print();
+
+  simpleQ.enqueue('Rob', 'Rob2', 'Rob3');
+  simpleQ.enqueue('John');
+  simpleQ.enqueue('Lol');
+  console.log('-------------');
+  simpleQ.front();
+  simpleQ.size();
+  simpleQ.isEmpty();
+
+  simpleQ.dequeue();
+  simpleQ.dequeue();
+  simpleQ.dequeue();
+  simpleQ.size();
+  simpleQ.isEmpty();
+  simpleQ.dequeue();
+  simpleQ.size();
+  console.log('-----END REGULAR QUEUE-----');
+}());
---
+++ stacks.js
@@ -0,0 +1,74 @@
+(function () {
+  'use strict';
+
+  //create a Stack 'class' with methods
+  function Stack() {
+    var theStack = [];
+
+    this.push = function () {
+      var i;
+      for (i = 0; i < arguments.length; i += 1) {
+        theStack.push(arguments[i]);
+        console.log('Pushed ' + arguments[i] + ' onto the stack.');
+      }
+    };
+
+    this.pop = function () {
+      var popparino = theStack.pop();
+      console.log("Popped off " + popparino + ".");
+      return popparino;
+    };
+
+    this.peek = function () {
+      console.log("The top of the stack:", theStack[theStack.length - 1]);
+      return theStack[theStack.length - 1];
+    };
+
+    this.isEmpty = function () {
+      console.log("Is the stack empty?:", (theStack.length === 0));
+      return theStack.length === 0;
+    };
+
+    this.clear = function () {
+      console.log("The Stack has been cleared.");
+      theStack = [];
+    };
+
+    this.size = function () {
+      console.log("There are " + theStack.length + " items in the stack.");
+      return theStack.length;
+    };
+
+  }
+
+  function decToBinary(num) {
+    var binHoldArr = new Stack(),
+      binString = "",
+      startNum = num;
+    while (num > 0) {
+      binHoldArr.push(num % 2);
+      num = Math.floor(num / 2);
+    }
+
+    while (!binHoldArr.isEmpty()) {
+      binString += binHoldArr.pop();
+    }
+
+    return "decimal: " + startNum + " binary: " + binString.trim();
+  }
+
+  var aNewStack = new Stack();
+  aNewStack.isEmpty();
+  aNewStack.push('Nanner', 3, 12, 4);
+  aNewStack.isEmpty();
+  aNewStack.size();
+  aNewStack.pop();
+  aNewStack.peek();
+  aNewStack.clear();
+  aNewStack.isEmpty();
+  console.log('----------------');
+  console.log(decToBinary(10));
+  console.log('----------------');
+  console.log(decToBinary(2031));
+
+}());
