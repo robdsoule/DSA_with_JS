3dd8846f7cba3bc99bb0f9c75a2330b83a2313c4
rsoule3 <robsoule3@gmail.com>
2015-07-07 20:54:05 -0400

    Doubly Linked list, finish out methods, toString, insertAt, indexOf, removeAt

--- bidirection_linked_list.js
+++ bidirection_linked_list.js
@@ -32,10 +32,113 @@
 
     };
 
-    //TODO removeAt taking position parameter
-    //TODO insertAt taking element and position parameters
-    //TODO indexOf a specific element
-    //TODO toString print list out in a legible manner
+    this.removeAt = function (position) {
+      var current, previous, next, index, removedElement;
+
+      current = head;
+      index = 0;
+
+      if (position > -1 && position < length) {
+        if (position === 0) {
+          head = current.next;
+          head.previous = null;
+          removedElement = current.element;
+          length -= 1;
+        } else {
+
+          while (index < position) {
+            index += 1;
+            previous = current;
+            current = current.next;
+          }
+
+          next = current.next;
+          if (next) {
+            next.previous = previous;
+          }
+          previous.next = next;
+          removedElement = current.element;
+          length -= 1;
+        }
+      } else {
+        removedElement = null;
+      }
+
+      return removedElement;
+    };
+
+    this.insertAt = function (element, position) {
+      var node, previous, current, index, wasInserted;
+      wasInserted = false;
+      index = 0;
+      current = head;
+
+      if (position > -1 && position <= length) {
+        node = new Node(element);
+
+        if (position === 0) {
+          current.previous = node;
+          node.next = current;
+          head = node;
+          wasInserted = true;
+          length += 1;
+        } else {
+
+          while (index < position) {
+            previous = current;
+            current = current.next;
+            index += 1;
+          }
+
+          node.next = current;
+          previous.next = node;
+          node.previous = previous;
+          wasInserted = true;
+          length += 1;
+        }
+      }
+
+      return wasInserted;
+    };
+
+    this.indexOf = function (element) {
+      var current, index, eleFound;
+      index = 0;
+      eleFound = false;
+      current = head;
+
+      if (head.element === element) {
+        eleFound = true;
+      } else {
+        while (!eleFound && current.next) {
+          current = current.next;
+          if (current.element === element) {
+            eleFound = true;
+          }
+          index += 1;
+        }
+      }
+      if (!eleFound) {
+        index = null;
+      }
+
+      return index;
+    };
+
+    this.toString = function () {
+      var current, index, output;
+      current = head;
+      output = 'BEGINNING:\n';
+      index = 0;
+
+      while (index < length) {
+        output += index + ' : ' + current.element + '\n';
+        current = current.next;
+        index += 1;
+      }
+
+      return output.trim();
+    };
 
     this.getHead = function () {
       return head;
@@ -56,7 +159,12 @@
   twoWayList.append('Item1');
   twoWayList.append('Item2');
   twoWayList.append('Item3');
+  console.log(twoWayList.removeAt(2));
+  twoWayList.insertAt('Inserted Elem', 0);
+  twoWayList.insertAt('Another Inserted Ele', 3);
+  console.log(twoWayList.indexOf('Another Inserted Ele'));
+  console.log(twoWayList.indexOf('Shouldnt be here'));
   console.log(twoWayList.getHead());
-  console.log(twoWayList);
+  console.log(twoWayList.toString());
 
 }());
