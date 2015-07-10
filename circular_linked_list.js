123ae722da9847d15c68720d5dbe2cf19ea58f53
rsoule3 <robsoule3@gmail.com>
2015-07-10 19:59:51 -0400

    circular linked list insert at, taking into account head/tail

--- circular_linked_list.js
+++ circular_linked_list.js
@@ -35,8 +35,84 @@
 
     };
 
+    this.indexOf = function (element) {
+      var current, index, elementFound;
+
+      elementFound = false;
+      current = head;
+      index = 0;
+
+      if (element === current.element) {
+        elementFound = true;
+      } else {
+        while (index < length && elementFound === false) {
+          current = current.next;
+          index += 1;
+          if (element === current.element) {
+            elementFound = true;
+          }
+        }
+      }
+      if (!elementFound) {
+        index = null;
+      }
+
+      return index;
+    };
+
+    this.insertAt = function (position, element) {
+      var current, previous, index, node;
+
+      index = 0;
+      current = head;
+      node = new Node(element);
+
+      if (position > -1 && position <= length) {
+        if (position === 0) {
+          node.next = current;
+          head = node;
+          tail.next = head;
+          head.prev = tail;
+        } else if (position === length) {
+          this.append(element);
+        } else {
+          while (index < position) {
+            previous = current;
+            current = current.next;
+            index += 1;
+          }
+          previous.next = node;
+          current.prev = node;
+          node.prev = previous;
+          node.next = current;
+        }
+        length += 1;
+      }
+    };
+
+    //this.removeAt = function (position) {
+    //  var current, previous, index, removedEle;
+    //  index = 0;
+    //
+    //  if (position > -1 && position < length) {
+    //
+    //  }
+    //};
+
+    this.size = function () {
+      return length;
+    };
+
+    this.isEmpty = function () {
+      return length === 0;
+    };
+
     this.getHead = function () {
       return head;
+    };
+
+    this.getTail = function () {
+      return tail;
     };
   }
 
@@ -44,5 +120,12 @@
   hotPotato.append('Rob1');
   hotPotato.append('Rob2');
   hotPotato.append('Rob3');
+  hotPotato.insertAt(3, 'Nanner');
+  hotPotato.insertAt(1, 'Test');
+  hotPotato.insertAt(0, 'Begin');
   console.log(hotPotato.getHead());
+  console.log('Index Of: ',
+              hotPotato.indexOf('Rob3'),
+              hotPotato.indexOf('Rob4'),
+              hotPotato.indexOf('Rob1'));
 }());
